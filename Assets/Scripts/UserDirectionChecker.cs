83fc0863d917ead9de966ac829cb9706fefec19a
Goodmanc1998 <goodmanc1998@gmail,com>
2021-03-30 16:57:42 +0100

    Small Tweeks
    Adjusting the viewing range of center, left and right

--- Assets/Scripts/UserDirectionChecker.cs
+++ Assets/Scripts/UserDirectionChecker.cs
@@ -14,6 +14,17 @@ public class UserDirectionChecker : MonoBehaviour
     public float centerViewTime;
     public float rightViewTime;
 
+    float leftAngle;
+    float rightAngle;
+    float frontAngle;
+    float rearAngle;
+
+    Vector3 cameraRot;
+    Vector3 carDir;
+
+
+    float camAngle;
+
     private void Awake()
     {
         GameManager.onGameEvent += UpdateScore;
@@ -28,19 +39,38 @@ public class UserDirectionChecker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float angle = Vector3.Angle(transform.forward, cameraTransform.forward);
 
-        if (cameraTransform.eulerAngles.y < transform.parent.eulerAngles.y - frontViewAngle)
-        {
-            leftViewTime += Time.deltaTime;
-        }
-        else if(cameraTransform.eulerAngles.y > transform.parent.eulerAngles.y + frontViewAngle)
-        {
-            rightViewTime += Time.deltaTime;
-        }
-        else
+        float diff =  transform.localRotation.y - cameraTransform.localRotation.y;
+
+
+
+
+
+        if (angle <= frontViewAngle)
         {
             centerViewTime += Time.deltaTime;
         }
+        else if(angle > frontAngle)
+        {
+            if(diff < 0)
+            {
+                rightViewTime += Time.deltaTime;
+            }
+            else if (diff > 0)
+            {
+                leftViewTime += Time.deltaTime;
+            }
+        }
+
+
+
+        Debug.Log("Angle : " + diff);
+
+        
+        
+
+        
 
     }
 
